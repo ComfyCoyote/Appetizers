9266325350668e7139e5a2f51ebac6955360132d
ComfyCoyote <muzzadamjee@gmail.com>
2024-12-06 10:05:11 -0800

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Muzzammil Adamjee on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
